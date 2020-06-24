23871671407c74444c508ea57c1b45bc20a7c05f
Christopher West <arynoyn@gmail.com>
2020-06-24 08:23:40 -0600

    Added audio for ball collisions and block destruction
    Added audio for ball collisions and block destruction

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class Ball : MonoBehaviour
 {
@@ -8,17 +12,22 @@ public class Ball : MonoBehaviour
     [SerializeField] private Paddle paddle1;
     [SerializeField] private float xInitialVelocity = 2f;
     [SerializeField] private float yInitialVelocity = 15f;
-    
+
+    [SerializeField] private AudioClip[] ballSounds;
     //state
     private Vector2 _paddleToBallVector;
     private Rigidbody2D _rigidBody;
     private bool _hasStarted;
+    
+    //Cached component references
+    private AudioSource _myAudioSource;
 
     // Start is called before the first frame update
     void Start() {
         _rigidBody = GetComponent<Rigidbody2D>();
         _paddleToBallVector = transform.position - paddle1.transform.position;
         _hasStarted = false;
+        _myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,5 +49,12 @@ public class Ball : MonoBehaviour
         var paddle1TransformPosition = paddle1.transform.position;
         var paddlePosition = new Vector2(paddle1TransformPosition.x, paddle1TransformPosition.y);
         transform.position = paddlePosition + _paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (_hasStarted) {
+            var clip = ballSounds[Random.Range(0, ballSounds.Length)];
+            _myAudioSource.PlayOneShot(clip);
+        }
     }
 }
--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class Block : MonoBehaviour
-{
+public class Block : MonoBehaviour {
+    [SerializeField] AudioClip breakSound;
+
     private void OnCollisionEnter2D(Collision2D collision) {
+        if (Camera.main != null) {
+            AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        }
+
         Destroy(gameObject);
     }
 }
