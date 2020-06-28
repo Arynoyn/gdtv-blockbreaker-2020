16f10972aff4e32a66ddaf37507f5ea210834c44
Christopher West <arynoyn@gmail.com>
2020-06-28 09:53:24 -0600

    Added the logic for the win condition of a level
    Added the logic for the win condition of a level

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,12 +6,25 @@ using UnityEngine.Serialization;
 
 public class Block : MonoBehaviour {
     [SerializeField] AudioClip breakSound;
+    
+    //cached reference
+    private Level level;
+
+    private void Start() {
+        level = FindObjectOfType<Level>();
+        level.IncreaseBreakableBlockCount();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision) {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock() {
         if (Camera.main != null) {
             AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         }
 
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+    [SerializeField] int breakableBlocks; //Serialized for debugging purposes
+    private SceneLoader sceneLoader;
+
+    private void Start() {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void IncreaseBreakableBlockCount() {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed() {
+        breakableBlocks--;
+        if (breakableBlocks <= 0) {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
